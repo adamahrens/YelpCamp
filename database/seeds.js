01c1cf447d8ebc0562ff3ec24cae346ab3697ea7
Adam Ahrens <adam.aahrens@gmail.com>
2019-02-14 09:57:23 -0600

    Using promises for handling the async nature of saves

--- database/seeds.js
+++ database/seeds.js
@@ -8,20 +8,17 @@ function populateDatabase() {
       console.log('Removed all comments and Campgrounds. Repopulating');
 
       // CREATE COMMENTS & CAMPGROUNDS
-      Campground.create({ name: "River Bottom", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", blurb: faker.lorem.paragraphs(3) }, function(error, obj) {
-        if (error) {
-          console.log("Error saving campground1 to database");
-        } else {
-          console.log('Saved ' + obj + ' to the database');
-        }
-      }).then(function(camp){
+      var promise1 = Campground.create({ name: "River Bottom", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", blurb: faker.lorem.paragraphs(3) });
+      promise1.then(function(camp) {
+        console.log('Saved campground1 to the database');
+
         Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
           if (error) {
             console.log("Error saving comment to campground1");
           } else {
-            console.log('Saved' + com + ' to the database');
             camp.comments.push(com);
             camp.save();
+            console.log('Added a comment to campground1');
           }
         });
 
@@ -29,35 +26,146 @@ function populateDatabase() {
           if (error) {
             console.log("Error saving comment to campground1");
           } else {
-            console.log('Saved' + com + ' to the database');
             camp.comments.push(com);
             camp.save();
+            console.log('Added a comment to campground1');
           }
         });
       });
 
-      Campground.create({ name: "Granite Hill", image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", blurb: faker.lorem.paragraphs(3) }, function(error, obj) {
-        if (error) {
-          console.log("Error saving campground2 to database");
-        } else {
-          console.log('Saved ' + obj + ' to the database');
-        }
+      var promise2 = Campground.create({ name: "Granite Hill", image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", blurb: faker.lorem.paragraphs(3) });
+      promise2.then(function(camp) {
+        console.log('Saved campground2 to the database');
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground2");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground2');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground2");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground2');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground2");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground2');
+          }
+        });
       });
 
-      Campground.create({ name: "Price Creek",  image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", blurb: faker.lorem.paragraphs(3) }, function(error, obj) {
-        if (error) {
-          console.log("Error saving campground3 to database");
-        } else {
-          console.log('Saved ' + obj + ' to the database');
-        }
+      var promise3 = Campground.create({ name: "Price Creek",  image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", blurb: faker.lorem.paragraphs(3) });
+      promise3.then(function(camp) {
+        console.log('Saved campground3 to the database');
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground3");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground3');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground3");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground3');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground3");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground3');
+          }
+        });
       });
 
-      Campground.create({ name: "Python Playground", image: "https://images.unsplash.com/photo-1480779735619-f73b30fdc062?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80", blurb: faker.lorem.paragraphs(3) }, function(error, obj) {
-        if (error) {
-          console.log("Error saving campground4 to database");
-        } else {
-          console.log('Saved ' + obj + ' to the database');
-        }
+      var promise4 = Campground.create({ name: "Python Playground", image: "https://images.unsplash.com/photo-1480779735619-f73b30fdc062?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80", blurb: faker.lorem.paragraphs(3) });
+      promise4.then(function(camp) {
+        console.log('Saved campground4 to the database');
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground4");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground4');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground4");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground4');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground4");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground4');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground4");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground4');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground4");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground4');
+          }
+        });
+
+        Comment.create({ text: faker.lorem.sentences(2) }, function(error, com) {
+          if (error) {
+            console.log("Error saving comment to campground4");
+          } else {
+            camp.comments.push(com);
+            camp.save();
+            console.log('Added a comment to campground4');
+          }
+        });
       });
     });
   });
